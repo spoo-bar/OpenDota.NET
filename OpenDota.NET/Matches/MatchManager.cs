d52e47a4356bc1f90bef869f167000fd04d3c11e
Spoorthi <spoorthi.s@proenco.com>
2018-09-24 14:34:11 +0530

    non parameter get public match wrapper

--- OpenDota.NET/Matches/MatchManager.cs
+++ OpenDota.NET/Matches/MatchManager.cs
@@ -60,6 +60,32 @@ namespace OpenDota.NET.Matches
             throw new Exception("Could not successfully get pro matches data");
         }
 
+        public IEnumerable<PublicMatch> GetPublicMatches()
+        {
+            return GetPublicMatchesAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<IEnumerable<PublicMatch>> GetPublicMatchesAsync()
+        {
+            var client = OpenDotaAPIWrapper.Client;
+            using (var response = await client.GetAsync("publicmatches"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var publicMatches = new List<PublicMatch>();
+                    foreach (var publicMatch in JArray.Parse(result))
+                    {
+                        publicMatches.Add(PublicMatch.Deserialize(publicMatch));
+                    }
+
+                    return publicMatches;
+                }
+            }
+            throw new Exception("Could not successfully get public matches data");
+        }
+
         [Obsolete("OpenDota endpoint is accessed over hundred times. Do not use unless you have an api key. Alternatively you can use GetProMatchesDetails and get details for required matches individually using GetMatch.", false)]
         public IEnumerable<ProMatch> GetProMatches()
         {
