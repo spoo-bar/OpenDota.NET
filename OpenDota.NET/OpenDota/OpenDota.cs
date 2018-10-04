828655e27220bf752f7f68e0f0a0f9f0f3272be1
Spoorthi <spoorthi.s@proenco.com>
2018-10-04 16:18:54 +0530

    api wrapper implementation for opendota metadata

---
+++ OpenDota.NET/OpenDota/OpenDota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OpenDota.NET
+{
+    public class OpenDotA
+    {
+        public static MetaData GetMetaData()
+        {
+            return GetMetaDataAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task<MetaData> GetMetaDataAsync()
+        {
+            var client = OpenDotaAPIWrapper.Client;
+            using (var response = await client.GetAsync("metadata"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return MetaData.Deserialize(result);
+                }
+            }
+            throw new Exception("Could not get meta data");
+        }
+    }
+}
